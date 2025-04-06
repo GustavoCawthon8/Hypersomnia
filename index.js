a9e551a060e97f0111c15a47596f3591f766778d
englishtemaedition <englishtemaedition@gmail.com>
2025-04-06 11:44:57 +0000

    atualizações

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-import express from 'express';
-import chalk from 'chalk';
-import axios from 'axios';
-import path from 'path';
-import cors from 'cors';
-import helmet from 'helmet';
+const axios = require('axios');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
 const porta = process.env.PORT || 7000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(helmet());
 app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, "pages", "index.html"));
+})
+
 app.post("/proxy", async (req, res) => {
   try {
     const { url, method, headers, body } = req.body;
@@ -22,10 +24,7 @@ app.post("/proxy", async (req, res) => {
   }
 });
 
-app.get("/", (req, res)=>{
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-})
 
 app.listen(porta, ()=>{
-  console.log(chalk.bgGreen(`Servidor rodando na porta http://localhost:${porta}`))
+  console.log(`Servidor rodando na porta http://localhost:${porta}`)
 })
